681885db2fa3b2903148b9b14b0d9edc8355d512
Chuck Millsap <cmmillsap4@gmail.com>
2018-08-21 23:52:39 -0400

    Added new print statement

--- Project1/src/dev1/HelloWorld.java
+++ Project1/src/dev1/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Donald Trump > Hillary Clinton");
+		System.out.println("Why do I have to be a part of generation snowflake?");
 	}
 
 }
